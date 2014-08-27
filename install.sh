ee3fb7e18d0cf4b2670a01476f63a02db9654936
Romain Gautier <mail@romain.sh>
2014-08-28 00:18:04 +0200

    simplify install.sh

--- install.sh
+++ install.sh
@@ -8,29 +8,12 @@ git submodule update --init
 # Enable pure for oh-my-zsh
 ln -sf $ROOT/zsh/pure/pure.zsh $ROOT/zsh/oh-my-zsh/custom/pure.zsh-theme
 
-function createZshrc {
-    echo "# github.com/mykiwi/dotfiles
+# Created .zshrc
+echo "# github.com/mykiwi/dotfiles
 export DOTFILES=$ROOT
 source \$DOTFILES/.zshrc" > ~/.zshrc
-}
 
-# Create the .zshrc only if the user want
-if [ -e ~/.zshrc ]
-then
+# Created symlink for gitconfig
+ln -sf $DOTFILES/.gitconfig ~/.gitconfig
 
-    if [ -L ~/.zshrc ]
-    then
-        echo "/!\\ You already have a ~/.zshrc and it's already a symlink. Do you want to reset it? [y/n]:"
-    else
-        echo "/!\\ The file ~/.zshrc already exists. Do you want to reset it? [y/n]:"
-    fi
-
-    read action
-    if [ "y" = "$action" ]
-    then
-        createZshrc
-    fi
-
-else
-    createZshrc
-fi
+echo Done
